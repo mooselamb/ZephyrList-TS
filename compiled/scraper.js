ee068fa48ba32fddfce6aee28c4ad4577d62a0d6
Umair Shafiq <soulofstorms@hotmail.com>
2024-02-22 10:30:05 -0500

    Functional API Updated

--- compiled/scraper.js
+++ compiled/scraper.js
@@ -1,105 +1,191 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
+import express from 'express';
+import 'dotenv/config';
+import * as puppeteer from "puppeteer";
+import fetch from "node-fetch";
+import * as fs from 'fs';
+const app = express();
+app.use(express.json());
+const port = 3000;
+app.post('/search', async (req, res) => {
+    const searchQuery = req.body.searchQuery;
+    // console.log("Search Query:", searchQuery);
+    if (!searchQuery) {
+        return res.status(400).send({ error: 'searchQuery is required' });
     }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
+    try {
+        const url = await searchForHomeListingWebsites(searchQuery);
+        if (url) {
+            res.send({ url });
+        }
+        else {
+            res.status(404).send({ error: 'No matching real estate website found.' });
+        }
+    }
+    catch (error) {
+        console.error("Error during search:", error);
+        res.status(500).send({ error: 'Internal server error' });
+    }
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+const searchForHomeListingWebsites = async (searchQuery) => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-web-security']
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+    await page.setRequestInterception(true);
+    // Check for image requests and abort them
+    page.on('request', interceptedRequest => {
+        if (interceptedRequest.resourceType() === 'image') {
+            interceptedRequest.abort();
+        }
+        else {
+            interceptedRequest.continue();
+        }
+    });
+    console.log("Search Query:", searchQuery);
+    //Go to Google.com and enter search query
+    await page.goto('https://www.google.com');
+    await page.waitForSelector('textarea[name="q"]');
+    await page.type('textarea[name="q"]', searchQuery);
+    await page.keyboard.press('Enter');
+    await page.waitForNavigation();
+    // const websites = [
+    //     'bienici.com', 'seloger.com', 'leboncoin.fr', 'rightmove.co.uk',
+    //     'craigslist.org', 'entreparticuliers.com', 'immo-diffusion.fr',
+    //     'paruvendu.fr', 'pap.fr', 'ouestfrance-immo.com', 'funda.nl',
+    //     'zillow.com', 'trulia.com', 'primeshop.com.hk', 'propertypassbook.com',
+    //     'apartments.com', 'realtor.com'
+    // ];
+    // const url = await page.evaluate((websites) => {
+    //     const links = Array.from(document.querySelectorAll('a'));
+    //     for (let link of links) {
+    //         const href = link.href;
+    //         if (websites.some(website => href.includes(website))) {
+    //             return href;
+    //         }
+    //     }
+    //     return '';
+    // }, websites);
+    let foundUrl = '';
+    const targetWebsite = 'zillow.com';
+    let found = false;
+    while (!found) {
+        await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+        const moreResultsSelector = '[aria-label="More results"]';
+        if (await page.$(moreResultsSelector) !== null) {
+            await page.click(moreResultsSelector);
+            // Wait for the next set of results to load
+            await page.waitForNavigation({ waitUntil: 'networkidle0' });
+        }
+        const links = await page.evaluate(() => Array.from(document.querySelectorAll('a')).map(link => link.href));
+        const targetLink = links.find(link => link.includes(targetWebsite));
+        if (targetLink) {
+            foundUrl = targetLink;
+            console.log(`Found ${targetWebsite}`);
+            found = true;
+            fetchRealEstateWebsiteData(targetLink);
+        }
+    }
+    if (!found) {
+        console.log(`${targetWebsite} not found after many attempts.`);
+    }
+    await browser.close();
+    return found ? foundUrl : '';
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+const fetchRealEstateWebsiteData = async (url) => {
+    const apiEndPoint = "http://api.scraping-bot.io/scrape/real-estate";
+    const auth = "Basic " + Buffer.from(`ushafiq:FKtDHM69KDK07nmrlKuQTWOW3`).toString("base64");
+    try {
+        const response = await fetch(apiEndPoint, {
+            method: "POST",
+            body: JSON.stringify({
+                url: url,
+                options: {
+                    useChrome: false,
+                    premiumProxy: true,
+                    proxyCountry: "US",
+                    waitForNetworkRequests: true,
+                }
+            }),
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": auth
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`Server responded with status ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("Data:", data);
+        if (data.data && data.data.results && data.data.results.length > 0) {
+            const listings = data.data.results.map((listing) => ({
+                price: listing.price,
+                address: listing.address,
+                bedrooms: listing.beds,
+                baths: listing.baths,
+                area: listing.area,
+            }));
+            fs.writeFile('listings.json', JSON.stringify({ listings: listings }, null, 2), 'utf8', (err) => {
+                if (err) {
+                    console.error("Error saving listings data", err);
+                    return;
+                }
+                console.log('Listings data has been saved.');
+            });
+        }
+        else {
+            console.log("No listings found or unable to parse listings.");
+        }
+    }
+    catch (error) {
+        console.error("Error fetching listings data", error);
+    }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const puppeteer = __importStar(require("puppeteer"));
-//Func to launch browser and create new page
-function setBrowser() {
-    return __awaiter(this, void 0, void 0, function* () {
-        console.log('Inside setBrowser');
-        const browser = yield puppeteer.launch({ headless: false });
-        const page = yield browser.newPage();
-        yield page.goto("https://www.zillow.com/homes/for_sale/");
-        return { browser, page };
-    });
-}
-//Func to navigate to URL
-function navigateToURL(page, url) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield page.goto(url, { waitUntil: "networkidle2" });
-            console.log(`Navigated to ${url}`);
-        }
-        catch (err) {
-            console.error(`Could not navigate to ${url}`, err);
-            throw err;
-        }
-    });
-}
-//Func to search for listings
-function searchListings(page, city) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield page.type("#__c11n_j1wmrge", city);
-        yield page.click("#__c11n_j1wmrgg");
-        yield page.waitForNavigation({ waitUntil: "networkidle2" });
-        console.log(`Searched for listings in ${city}`);
-    });
-}
-//Scraper func
-function scrapeListings() {
-    return __awaiter(this, void 0, void 0, function* () {
-        console.log('Inside scrapeListings');
-        let browser, page;
-        try {
-            const result = yield setBrowser();
-            browser = result.browser;
-            page = result.page;
-            // await page.click('button', { delay: 7000 })
-            console.log('Page was set');
-            const input = yield page.$('input');
-            yield (input === null || input === void 0 ? void 0 : input.type('sylvania'));
-            // const listings = await page.evaluate(() => {
-            //     console.log('Evaluating listings')
-            //     const listings = document.querySelectorAll(".list-card-info");
-            //     console.log(listings)
-            //     const listingArray = Array.from(listings).map((listing) => {
-            //         const details = listing.textContent?.split("•");
-            //         return {
-            //             price: details?.[0],
-            //             address: details?.[1],
-            //             broker: details?.[2]
-            //         };
-            //     });
-            //     return listingArray;
-            // });
-        }
-        catch (err) {
-            console.log('Error scraping listings', err);
-            throw err;
-        }
-        finally {
-            // if (browser) browser?.close();
-        }
-    });
-}
-scrapeListings().catch(console.error);
+// searchForHomeListingWebsites("top home listing websites zillow austin tx");
+// // async function crawlGoogle() {
+// //     const browser = await puppeteer.launch({headless: false});
+// //     const page = await browser.newPage();
+// //     await page.setRequestInterception(true); 
+// //     // Check for image requests and abort them
+// //     page.on('request', interceptedRequest => { 
+// //         if ( 
+// //             interceptedRequest.url().endsWith('.png') || 
+// //             interceptedRequest.url().endsWith('.jpg') || 
+// //             interceptedRequest.url().includes('.png?') || 
+// //             interceptedRequest.url().includes('.jpg?') 
+// //         ) { 
+// //             interceptedRequest.abort(); 
+// //         } else { 
+// //             interceptedRequest.continue(); 
+// //         } 
+// //     }); 
+// //     //Go to Google.com and enter search query
+// //     await page.goto('https://www.google.com', {waitUntil: 'networkidle2'});
+// //     await page.type('textarea[name="q"]', 'top home listing websites');
+// //     await page.keyboard.press('Enter');
+// //     await page.waitForNavigation({waitUntil: 'networkidle2'});
+// //     const homeListingWebsites = await page.evaluate(() => {
+// //         const links: any = [];
+// //         const el = document.querySelectorAll('a[href]');
+// //         el.forEach((a) => {
+// //             const href = a.getAttribute('href');
+// //             if (href && href.startsWith('http')) {
+// //                 links.push(href);
+// //             }
+// //         });
+// //         return links;
+// //     });
+// //     console.log("Link Results:", homeListingWebsites);
+// //     if (homeListingWebsites.length > 0) {
+// //         const realEstateWebsiteUrl = homeListingWebsites[0];
+// //         await fetchRealEstateWebsiteData(realEstateWebsiteUrl);
+// //     } else {
+// //         console.log("No real estate website found");
+// //     }
+// //     // await browser.close();
+// // }
--- scraper.ts
+++ scraper.ts
@@ -1,80 +1,162 @@
+import 'dotenv/config';
 import * as puppeteer from "puppeteer";
+import fetch from "node-fetch";
 
-//Func to launch browser and create new page
-async function setBrowser() {
-    console.log('Inside setBrowser')
-    const browser = await puppeteer.launch({headless:false});
 
+
+const searchForHomeListingWebsites = async () => {
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-    await page.goto("https://www.zillow.com/homes/for_sale/");
+    await page.setRequestInterception(true); 
+ 
+    // Check for image requests and abort them
+    page.on('request', interceptedRequest => { 
+        if ( 
+            interceptedRequest.url().endsWith('.png') || 
+            interceptedRequest.url().endsWith('.jpg') || 
+            interceptedRequest.url().includes('.png?') || 
+            interceptedRequest.url().includes('.jpg?') 
+        ) { 
+            interceptedRequest.abort(); 
+        } else { 
+            interceptedRequest.continue(); 
+        } 
+    }); 
 
-   return {browser,page};
-}
+    //Go to Google.com and enter search query
+    await page.goto('https://www.google.com');
+    await page.type('textarea[name="q"]', 'top home listing websites');
+    await page.keyboard.press('Enter');
+    await page.waitForNavigation();
 
-//Func to navigate to URL
-async function navigateToURL(page: puppeteer.Page, url: string) {
-    try {
-        await page.goto(url,{waitUntil: "networkidle2"});
+    const websites = [
+        'bienici.com', 'seloger.com', 'leboncoin.fr', 'rightmove.co.uk',
+        'craigslist.org', 'entreparticuliers.com', 'immo-diffusion.fr',
+        'paruvendu.fr', 'pap.fr', 'ouestfrance-immo.com', 'funda.nl',
+        'zillow.com', 'trulia.com', 'primeshop.com.hk', 'propertypassbook.com',
+        'apartments.com', 'realtor.com'
+    ];
 
-        console.log(`Navigated to ${url}`);
+    const url = await page.evaluate((websites) => {
+        const links = Array.from(document.querySelectorAll('a'));
+        for (let link of links) {
+            const href = link.href;
+            if (websites.some(website => href.includes(website))) {
+                return href;
+            }
+        }
+        return '';
+    }, websites);
 
-    }   catch (err) {
-        console.error(`Could not navigate to ${url}`, err);
+    await browser.close();
 
-        throw err;
+    if (url) {
+        console.log(`Found URL: ${url}`);
+        fetchRealEstateWebsiteData(url);
+    } else {
+        console.log("No matching real estate website found.");
     }
-}
+};
 
-//Func to search for listings
-async function searchListings(page: puppeteer.Page, city: string) {
-    await page.type("#__c11n_j1wmrge", city);
-    await page.click("#__c11n_j1wmrgg");
-    await page.waitForNavigation({waitUntil: "networkidle2"});
-    console.log(`Searched for listings in ${city}`);
-}
 
-//Scraper func
-async function scrapeListings() {
-    console.log('Inside scrapeListings')
-    let browser, page;
+const fetchRealEstateWebsiteData = async (url) => {
+    const apiEndPoint = "http://api.scraping-bot.io/scrape/real-estate";
+    const auth = "Basic " + Buffer.from(`ushafiq:FKtDHM69KDK07nmrlKuQTWOW3`).toString("base64");
+
     try {
-        const result = await setBrowser();
-        browser = result.browser;
-        page = result.page;
-
-        // await page.click('button', { delay: 7000 })
-
-        console.log('Page was set')
-        const input = await page.$('input')
-        await input?.type('sylvania')
+        const response = await fetch(apiEndPoint, {
+            method: "POST",
+            body: JSON.stringify({
+                url: url,
+                options: {
+                    useChrome: false,
+                    premiumProxy: true,
+                    proxyCountry: "US",
+                    waitForNetworkRequests: true,
+                }
+            }),
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": auth
+            }    
+        });
+        if (!response.ok) {
+            throw new Error(`Server responded with status ${response.status}`);
+        }
+        const data = await response.json();
         
-        // const listings = await page.evaluate(() => {
-        //     console.log('Evaluating listings')
-        //     const listings = document.querySelectorAll(".list-card-info");
-        //     console.log(listings)
-            
-        //     const listingArray = Array.from(listings).map((listing) => {
-        //         const details = listing.textContent?.split("•");
-                
-        //         return {
-        //             price: details?.[0],
-        //             address: details?.[1],
-        //             broker: details?.[2]
-        //         };
-        //     });
-        //     return listingArray;
-        // });
-
-    } catch (err) {
-        console.log('Error scraping listings', err);
-        throw err;
-    } finally {
-        // if (browser) browser?.close();
+        if (data.data && data.data.results && data.data.results.length > 0) {
+            const listing = data.data.results;
+            listing.forEach((listing, index) => {
+                console.log(`Listing ${index + 1}:`);
+                console.log(`Price: ${listing.price}`);
+                console.log(`Address: ${listing.address}`);
+                console.log(`Number of Bedrooms: ${listing.beds}`);
+                console.log(`Number of Baths: ${listing.baths}`);
+                console.log(`Area: ${listing.area} sqft`);
+                console.log('---'); 
+            });
+        } else {
+            console.log("No listings found");
+        }
+    } catch (error) {
+        console.error("Error fetching listings data", error);
     }
 }
 
+fetchRealEstateWebsiteData();
 
 
-scrapeListings().catch(console.error);
+
+// // async function crawlGoogle() {
+// //     const browser = await puppeteer.launch({headless: false});
+// //     const page = await browser.newPage();
+
+// //     await page.setRequestInterception(true); 
+ 
+// //     // Check for image requests and abort them
+// //     page.on('request', interceptedRequest => { 
+// //         if ( 
+// //             interceptedRequest.url().endsWith('.png') || 
+// //             interceptedRequest.url().endsWith('.jpg') || 
+// //             interceptedRequest.url().includes('.png?') || 
+// //             interceptedRequest.url().includes('.jpg?') 
+// //         ) { 
+// //             interceptedRequest.abort(); 
+// //         } else { 
+// //             interceptedRequest.continue(); 
+// //         } 
+// //     }); 
+
+// //     //Go to Google.com and enter search query
+// //     await page.goto('https://www.google.com', {waitUntil: 'networkidle2'});
+// //     await page.type('textarea[name="q"]', 'top home listing websites');
+// //     await page.keyboard.press('Enter');
+// //     await page.waitForNavigation({waitUntil: 'networkidle2'});
+
+
+// //     const homeListingWebsites = await page.evaluate(() => {
+// //         const links: any = [];
+// //         const el = document.querySelectorAll('a[href]');
+// //         el.forEach((a) => {
+// //             const href = a.getAttribute('href');
+// //             if (href && href.startsWith('http')) {
+// //                 links.push(href);
+// //             }
+// //         });
+// //         return links;
+// //     });
+// //     console.log("Link Results:", homeListingWebsites);
+
+
+// //     if (homeListingWebsites.length > 0) {
+// //         const realEstateWebsiteUrl = homeListingWebsites[0];
+// //         await fetchRealEstateWebsiteData(realEstateWebsiteUrl);
+// //     } else {
+// //         console.log("No real estate website found");
+// //     }
+// //     // await browser.close();
+// // }
 
